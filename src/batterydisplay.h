a13b9791387f9605c229bf539f56fbe641726722
DunkelAmber <frama95@gmail.com>
2018-04-25 10:02:40 +0200

    bugfix

--- src/batterydisplay.h
+++ src/batterydisplay.h
@@ -4,6 +4,9 @@
 #include <QWidget>
 #include <QPainter>
 
+#include "batteryindicator.h"
+#include "batterypanel.h"
+
 #define BAT_DISPLAY_POSX 700
 #define BAT_DISPLAY_POSY 300
 #define BAT_DISPLAY_WIDTH 500
@@ -12,10 +15,19 @@
 #define COL_SPACER 10
 #define ROW_SPACER 10
 
-class BatteryDisplay
+class BatteryDisplay : public QWidget
 {
-public:
+		Q_OBJECT
+	public:
     BatteryDisplay();
+	signals:
+
+	public slots:
+
+protected:
+		 void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+private:
+		 //BatteryIndicator *battery1, *battery2, *battery3, *battery4, *battery5, *battery6, *battery7, *battery8, *battery9, *battery10;
 };
 
 #endif // BATTERYDISPLAY_H
