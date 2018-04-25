3c71513534640eaf62e57235f451539dd430169e
Francesco M <frama95@gmail.com>
2018-04-25 10:50:52 +0200

    add rect draw

--- src/batterydisplay.cpp
+++ src/batterydisplay.cpp
@@ -7,7 +7,7 @@ BatteryDisplay::BatteryDisplay(QWidget *parent) : QWidget(parent)
 
 void BatteryDisplay::paintEvent(QPaintEvent *)
 {
-	/*
+
 	QPainter painter(this);
 
 	painter.setPen(QColor(255,255,255));
@@ -15,5 +15,5 @@ void BatteryDisplay::paintEvent(QPaintEvent *)
 
 	painter.setBrush(QColor(0,0,0,0));
 	painter.drawRect(0, 0, BAT_PANEL_WIDTH, BAT_PANEL_HEIGHT);
-	*/
+	
 }
